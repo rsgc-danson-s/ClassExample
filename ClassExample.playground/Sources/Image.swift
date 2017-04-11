d156f043281842ed601898feae80a01a1aeafa08
Student <student@macbook-pro-0863-sebastian-danson.local>
2017-04-11 10:35:30 -0400

    nn

--- ClassExample.playground/Sources/Image.swift
+++ ClassExample.playground/Sources/Image.swift
@@ -16,8 +16,95 @@ public class Image {
         c = theCanvas
         
         // Draw the image by invoking method(s) below
+        c.translate(byX: -100, byY: 0)
+        
         upperCircle()
         lowerCircle()
+        lines()
+        
+        c.translate(byX: 100, byY: 0)
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        
+        c.translate(byX: 100, byY: 0)
+        
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        
+        c.translate(byX: 100, byY: 0)
+        
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: 100, byY: 0)
+        
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: 100, byY: 0)
+        
+        upperCircle()
+        lowerCircle()
+        lines()
+
+        
+        c.rotate(by: 180)
+
+        
+        c.translate(byX: -100, byY: -650)
+        
+
+        
+        upperCircle()
+        lowerCircle()
+        lines()
+
+        
+        c.translate(byX: 400, byY: 0)
+        
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: -100, byY: 0)
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: -100, byY: 0)
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: -100, byY: 0)
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: -100, byY: 0)
+        upperCircle()
+        lowerCircle()
+        lines()
+        
+        c.translate(byX: -100, byY: 0)
+        upperCircle()
+        lowerCircle()
+        lines()
+       
+        
+        
+    
+        
+    
+        
+        
+        
     }
     
     // MARK: Method(s)/Function(s)
@@ -29,9 +116,24 @@ public class Image {
      */
     func upperCircle() {
         
-        // Draw a circle in the middle of the canvas
-        c.drawEllipse(centreX: c.width / 2, centreY: c.height / 2, width: 100, height: 100)
-        c.drawText(message: "This circle is drawn from inside the Image class.", size: 12, x: 125, y: c.height / 2 - 100)
+        //draw top inner circle
+        //DRAW CIRCLES
+        //draw top inner circle
+        c.drawShapesWithBorders = true
+        c.drawShapesWithFill = false
+        c.defaultBorderWidth = 10
+        c.borderColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
+        c.drawEllipse(centreX: 50, centreY: 450, width: 70, height: 70)
+        
+        //draw top middle circle
+        c.borderColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
+        c.drawEllipse(centreX: 50, centreY: 450, width: 50, height:50)
+        
+        //draw top outer circle
+        c.borderColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
+        c.drawEllipse(centreX: 50, centreY: 450, width: 30, height:30)
+        
+        
         
     }
     
@@ -40,6 +142,50 @@ public class Image {
      */
     func lowerCircle() {
         
+        //draw bottom inner circle
+        c.drawShapesWithBorders = true
+        c.drawShapesWithFill = false
+        c.defaultBorderWidth = 10
+        c.borderColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
+        c.drawEllipse(centreX: 100, centreY: 375, width: 70, height: 70)
+        
+        //draw bottom middle circle
+        c.borderColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
+        c.drawEllipse(centreX: 100, centreY: 375, width: 50, height:50)
+        
+        //draw bottom outer circle
+        c.borderColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
+        
+        c.drawEllipse(centreX: 100, centreY: 375, width: 30, height:30)
+        
+    }
+    
+    func lines()  {
+        
+        // DRAW LINES
+        
+        //draw brown lines
+        c.lineColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
+        c.defaultLineWidth = 10
+        c.drawLine(fromX: 15, fromY: 450, toX: 15, toY: 375)
+        c.drawLine(fromX: 85, fromY: 450, toX: 85, toY: 375)
+        
+        
+        //draw orange lines
+        c.lineColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
+        c.drawLine(fromX: 25, fromY: 450, toX: 25, toY: 375)
+        c.drawLine(fromX: 75, fromY: 450, toX: 75, toY: 375)
+        
+        //draw yellow lines
+        c.lineColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
+        c.lineColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
+        c.drawLine(fromX: 35, fromY: 450, toX: 35, toY: 375)
+        c.drawLine(fromX: 65, fromY: 450, toX: 65, toY: 375)
+        //draw white lines
+        c.lineColor = Color.white
+        c.defaultLineWidth = 20
+        c.drawLine(fromX: 50, fromY: 440, toX: 50, toY: 400)
+        c.drawLine(fromX: 100, fromY: 435, toX: 100, toY: 385)
     }
     
     
